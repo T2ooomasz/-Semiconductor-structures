df48b5f98f73d9e989cb279972be7fca10ddfdd9
Janusz Twardak <ende.pld@gmail.com>
2022-04-26 13:47:02 +0200

    Added fuction plotting Eg as a function on T

--- Semiconductors.py
+++ Semiconductors.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import List, Optional, Mapping
+from typing import List, Optional, Mapping, Union
 import os
 
 from regex import A
@@ -15,7 +15,9 @@ class Perovskite:
         arg_start: Optional[float] = 0,
         arg_stop: Optional[float] = 1,
     ):
-        self.arguments = np.linspace(arg_start, arg_stop, resolution)
+        self.arguments = (
+            np.linspace(arg_start, arg_stop, resolution) if resolution > 1 else 0.5
+        )
         self.compound_1 = compound_1
         self.compound_2 = compound_2
 
@@ -24,11 +26,16 @@ class Perovskite:
         parameter_1: float,
         parameter_2: float,
         bowing: Optional[float] = 0,
+        constant: Optional[float] = 0,
+        arguments: Optional[Union[List[float], bool]] = None,
     ) -> List[float]:
 
+        if arguments is None:
+            arguments = self.arguments
+
         return [
-            parameter_1 * x + parameter_2 * (1 - x) + bowing * (1 - x) * x
-            for x in self.arguments
+            parameter_1 * x + parameter_2 * (1 - x) + bowing * (1 - x) * x + constant
+            for x in arguments
         ]
 
     def draw_graphs(
@@ -55,7 +62,7 @@ class Perovskite:
     def draw_Eg(
         self,
         bowing: Optional[float] = 0,
-        save: Optional[bool] = True,
+        save: Optional[bool] = False,
     ):
 
         Eg = self.interpolate(self.compound_1["Eg"], self.compound_2["Eg"], bowing)
@@ -72,6 +79,39 @@ class Perovskite:
             plt.show()
         plt.clf()
 
+    def draw_Eg_temp(
+        self,
+        temp_start: Optional[int] = 250,
+        temp_stop: Optional[int] = 350,
+    ):
+
+        alpha = self.interpolate(
+            self.compound_1["alpha"], self.compound_2["alpha"], arguments=[0.5]
+        )[0]
+        temperatures = np.linspace(temp_start, temp_stop, 1000)
+
+        Eg_0 = (
+            self.interpolate(
+                self.compound_1["Eg"], self.compound_2["Eg"], arguments=[0.5]
+            )[0]
+            + 250 * alpha
+        )
+
+        Eg_temp = self.interpolate(
+            parameter_1=alpha,
+            constant=Eg_0,
+            arguments=temperatures,
+            parameter_2=0,
+            bowing=0,
+        )
+
+        plt.plot(temperatures, Eg_temp)
+        plt.title("Eg(T), Cs Sn [C_l3x I_3(1-x)] for x=0.5")
+        plt.ylabel("Eg [eV]")
+        plt.xlabel("T [K]")
+        plt.show()
+        plt.clf()
+
 
 def main():
 
@@ -84,6 +124,7 @@ def main():
         "mh": 0.140,
         "Ep": 34.7,
         "a": 5.560,
+        "alpha": 0.7 * 0.001,  # eV/K
     }
 
     CsSnI3 = {
@@ -95,14 +136,11 @@ def main():
         "mh": 0.069,
         "Ep": 29.9,
         "a": 6.219,
+        "alpha": 0.35 * 0.001,  # eV/K
     }
 
-    perovskite = Perovskite(compound_1=CsSnCl3, compound_2=CsSnI3, resolution=1000)
-
-    perovskite.draw_graphs(bowing=0, save=True)
-    perovskite.draw_graphs(bowing=2, save=True)
-    perovskite.draw_Eg(bowing=0, save=True)
-    perovskite.draw_Eg(bowing=2, save=True)
+    perovskite = Perovskite(compound_1=CsSnCl3, compound_2=CsSnI3, resolution=1)
+    perovskite.draw_Eg_temp()
 
 
 if __name__ == "__main__":
