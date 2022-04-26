433a29a6033d5f298b78b4594170f3f8b590c586
Janusz Twardak <ende.pld@gmail.com>
2022-04-26 17:09:20 +0200

    Added bands plotting

--- Semiconductors.py
+++ Semiconductors.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from typing import List, Optional, Mapping, Union
+from typing import List, Optional, Mapping, Union, Dict
 import os
 
 from regex import A
@@ -20,6 +20,7 @@ class Perovskite:
         )
         self.compound_1 = compound_1
         self.compound_2 = compound_2
+        self.H_REDUCED = 6.582119569  # [eV]
 
     def interpolate(
         self,
@@ -81,36 +82,167 @@ class Perovskite:
 
     def draw_Eg_temp(
         self,
-        temp_start: Optional[int] = 250,
-        temp_stop: Optional[int] = 350,
-    ):
+        temperatures: List[float],
+        Eg_temp: List[float],
+        save: Optional[bool] = True,
+    ) -> None:
 
-        alpha = self.interpolate(
-            self.compound_1["alpha"], self.compound_2["alpha"], arguments=[0.5]
-        )[0]
-        temperatures = np.linspace(temp_start, temp_stop, 1000)
+        plt.plot(temperatures, Eg_temp)
+        plt.title("Eg(T), Cs Sn [C_l3x I_3(1-x)] for x=0.5")
+        plt.ylabel("Eg [eV]")
+        plt.xlabel("T [K]")
+
+        if save:
+            if not os.path.isdir("graphs2"):
+                os.makedirs("graphs2")
+            plt.savefig(str(f"graphs2/Eg_as_a_function_of_T_x_0,5.png"))
+        else:
+            plt.show()
+        plt.clf()
+
+    def calculate_Eg_temp(
+        self,
+        temperatures: List[float],
+        mix_parameter: float,
+        mixed_params: Dict[str, float],
+    ) -> List[float]:
 
         Eg_0 = (
             self.interpolate(
-                self.compound_1["Eg"], self.compound_2["Eg"], arguments=[0.5]
+                self.compound_1["Eg"], self.compound_2["Eg"], arguments=[mix_parameter]
             )[0]
-            + 250 * alpha
+            + 250 * mixed_params["alpha"]
         )
 
-        Eg_temp = self.interpolate(
-            parameter_1=alpha,
+        Eg_T = self.interpolate(
+            parameter_1=mixed_params["alpha"],
             constant=Eg_0,
             arguments=temperatures,
             parameter_2=0,
             bowing=0,
         )
 
-        plt.plot(temperatures, Eg_temp)
-        plt.title("Eg(T), Cs Sn [C_l3x I_3(1-x)] for x=0.5")
-        plt.ylabel("Eg [eV]")
+        return Eg_T
+
+    def draw_bands(
+        self,
+        temp_start: Optional[int] = 250,
+        temp_stop: Optional[int] = 350,
+        resolution: Optional[int] = 1000,
+        mix_parameter: Optional[float] = 0.5,
+        save: Optional[bool] = True,
+    ) -> None:
+
+        mixed_params = self.calculate_mixed_params(mix_parameter)
+        temperatures = np.linspace(temp_start, temp_stop, resolution, mix_parameter)
+        Eg_temp = self.calculate_Eg_temp(temperatures, mix_parameter, mixed_params)
+
+        self.draw_Eg_temp(temperatures, Eg_temp)
+
+        VB = self.bands_calculate_VB(Eg_temp, mixed_params)
+        plt.plot(temperatures, VB, label="VB", alpha=0.7)
+        plt.title("bands(T), Cs Sn [C_l3x I_3(1-x)] for x=0.5")
+        plt.ylabel("E [eV]")
         plt.xlabel("T [K]")
-        plt.show()
+
+        CH = self.bands_calculate_CH(Eg_temp, mixed_params)
+        plt.plot(temperatures, CH, label="CH", alpha=1, linewidth=2.0)
+
+        CL = self.bands_calculate_CH(Eg_temp, mixed_params)
+        plt.plot(
+            temperatures, CL, label="CL", alpha=1, linewidth=1.5, linestyle="dashed"
+        )
+
+        CS = self.bands_calculate_CS(Eg_temp, mixed_params)
+        plt.plot(temperatures, CS, label="CS", alpha=1, linewidth=1.5, linestyle="-")
+
+        plt.legend()
+        if save:
+            if not os.path.isdir("graphs2"):
+                os.makedirs("graphs2")
+            plt.savefig(str(f"graphs2/bands_x_0,5.png"))
+            plt.show()
+        else:
+            plt.show()
         plt.clf()
+
+    def calculate_mixed_params(self, mix_parameter) -> Dict[str, float]:
+
+        mixed_params = {
+            parameter: self.interpolate(
+                self.compound_1[parameter],
+                self.compound_2[parameter],
+                arguments=[mix_parameter],
+            )[0]
+            for parameter in self.compound_1.keys()
+        }
+        return mixed_params
+
+    def bands_calculate_VB(
+        self, Eg_temp: List[float], param: Dict[str, float]
+    ) -> List[float]:
+
+        return [
+            Eg
+            + (
+                self.H_REDUCED
+                / 2
+                * param["mh"]
+                * (
+                    1 / param["mh"]
+                    - param["Ep"] / 3 * (2 / Eg + 1 / (Eg + param["delta"]))
+                )
+                * 2
+            )
+            for Eg in Eg_temp
+        ]
+
+    def bands_calculate_CH(
+        self, Eg_temp: List[float], param: Dict[str, float]
+    ) -> List[float]:
+
+        return [
+            self.H_REDUCED
+            / (2 * param["mh"])
+            * (
+                (
+                    (param["gamma_1"] - 1 / 3 * (param["Ep"] / Eg))
+                    + (param["gamma_2"])
+                    - 1 / 6 * (param["Ep"] / Eg)
+                )
+                * 2
+            )
+            for Eg in Eg_temp
+        ]
+
+    def bands_calculate_CL(
+        self, Eg_temp: List[float], param: Dict[str, float]
+    ) -> List[float]:
+
+        return [
+            self.H_REDUCED
+            / (2 * param["mh"])
+            * (
+                (
+                    (param["gamma_1"] - 1 / 3 * (param["Ep"] / Eg))
+                    - (param["gamma_2"])
+                    - 1 / 6 * (param["Ep"] / Eg)
+                )
+                * 2
+            )
+            for Eg in Eg_temp
+        ]
+
+    def bands_calculate_CS(
+        self, Eg_temp: List[float], param: Dict[str, float]
+    ) -> List[float]:
+
+        return [
+            self.H_REDUCED
+            / (2 * param["mh"])
+            * ((param["gamma_1"] - 1 / 3 * (param["Ep"] / Eg)) * 2 - param["delta"])
+            for Eg in Eg_temp
+        ]
 
 
 def main():
@@ -140,7 +272,7 @@ def main():
     }
 
     perovskite = Perovskite(compound_1=CsSnCl3, compound_2=CsSnI3, resolution=1)
-    perovskite.draw_Eg_temp()
+    perovskite.draw_bands()
 
 
 if __name__ == "__main__":
