7208ed09f06775183ff46028af560d01533d9904
T2ooomasz <tomkazulak@gmail.com>
2022-05-30 14:50:22 +0200

    Make Per_w_temp a child class from perovskite. add Zadanie3 as before

--- SemiconductorsTK.py
+++ SemiconductorsTK.py
@@ -5,12 +5,12 @@ import os
 from regex import A
 
 from Interpolate import Interpolate as I
-from Perovskite import Perovskite
+from Perovskite import Perovskite as P
 from CsSnCl3 import CsSnCl3
 from CsSnI3 import CsSnI3
 
 from Zadanie2 import Zadanie2
-from Perovskite_With_Temperature import Perivskite_With_Temperature as Perovskite_wT
+from Perovskite_With_Temperature import Perivskite_With_Temperature as Perovskite
 
 
 def main():
@@ -18,17 +18,18 @@ def main():
     compound_2 = CsSnI3.CsSnI3
     mix_proportion=0.35
     
-    perovskite = Perovskite(compound_1=compound_1, compound_2=compound_2,  mix_proportion=mix_proportion)
+    #perovskite = Perovskite(compound_1=compound_1, compound_2=compound_2,  mix_proportion=mix_proportion)
 
-    perovskite1 = Perovskite_wT(perovskite=perovskite, temperature=250)
+    perovskite1 = Perovskite(compound_1=compound_1, compound_2=compound_2,  mix_proportion=mix_proportion, temperature=250)
     print(
         perovskite1.Eg_with_temperature,
         perovskite1.E_VB_with_temperature,
         perovskite1.E_CH_with_temperature,
         perovskite1.E_CL_with_temperature,
         perovskite1.E_CS_with_temperature)
-        
-    perovskite1.draw_bands(temp_start=200, temp_stop=300)
+
+    print(perovskite1.perovskite)    
+    #perovskite1.draw_bands(temp_start=200, temp_stop=300)
     
     
 
